5b0fb2c7d89496db6f579033281f4456d148436b
Matt Sousa <matt.sousa241@gmail.com>
2025-10-25 21:13:41 -0400

    tsconfig: include .d.ts; add next-pwa shim

--- src/types/next-pwa.d.ts
+++ src/types/next-pwa.d.ts
@@ -1,1 +1,11 @@
-declare module 'next-pwa';
+// src/types/next-pwa.d.ts
+declare module 'next-pwa/register' {
+  const mod: any;
+  export default mod;
+}
+
+declare module 'next-pwa' {
+  const mod: any;
+  export default mod;
+}
+
